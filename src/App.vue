0cd6da98f819f726cd8efe18084942d4a8ff0369
Oscar Espinoza <oscaranto.espinoza@gmail.com>
2023-09-01 00:08:33 -0400

    Fix(Home): google font added for font-family Lato and Monserrat, Home component created

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './views/Home.vue'
 </script>
 
 <template>
@@ -11,20 +12,28 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div> -->
-  <HelloWorld msg="Vite + Vue" />
+  <Home msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<style>
+:root {
+  --yelow: #ECA539;
+  --dark-red: #C00E20;
+  --light-red: #F22539;
+  --black: #353535;
+  --dark-gray: #5E5E5E;
+  --gray: #BFBFBF;
+  --light-gray: #E8E8E8;
+  --snow-white: #F5F5F5;
+  --white: #F9F9F9;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+body {
+  font-family: 'Lato', sans-serif;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container home">
+    <figure class="home__logo">
+      <PikachuIcon />
+    </figure>
+
+    <div class="home__description">
+      <h1>Welcome to Pokédex</h1>
+
+      <p>
+        The digital encyclopedia created by Professor Oak is an invaluable tool
+        to Trainers in the Pokémon world.
+      </p>
+
+      <button>Get started</button>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import PikachuIcon from '../components/icons/PikachuIcon.vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<style scoped>
+.home {
+  display: grid;
+  place-content: center;
+  gap: 60px;
+  min-height: 100vh;
+  color: var(--black);
+}
+.home__logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.home__description {
+  text-align: center;
+}
+.home__description h1 {
+  margin: 0 0 1.875rem;
+}
+.home__description p {
+  margin: 0 0 3.125rem;
+}
+</style>
