0862b5454290e390267b129194342971007a147f
Oscar Espinoza <oscaranto.espinoza@gmail.com>
2023-09-02 02:26:32 -0400

    Fix: Native router added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,44 @@
-<script setup>
-import AllList from './views/AllList.vue';
-// import Home from './views/Home.vue';
-</script>
-
 <template>
-  <!-- <Home /> -->
-  <AllList />
+  <component :is="currentView" />
 </template>
+
+<script setup>
+import { computed, onMounted, ref } from 'vue';
+
+import AllList from './views/AllList.vue';
+import Home from './views/Home.vue';
+import FavoriteList from './views/FavoriteList.vue';
+
+const routes = {
+  '/': Home,
+  '/all': AllList,
+  '/favorite': FavoriteList,
+};
+
+const currentPath = ref(window.location.hash)
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || 'NotFound'
+})
+
+onMounted(() => {
+  window.addEventListener('hashchange', () => {
+    currentPath.value = window.location.hash
+  })
+})
+</script>
 
 <style>
 :root {
-  --yelow: #ECA539;
-  --dark-red: #C00E20;
-  --light-red: #F22539;
+  --yelow: #eca539;
+  --dark-red: #c00e20;
+  --light-red: #f22539;
   --black: #353535;
-  --dark-gray: #5E5E5E;
-  --gray: #BFBFBF;
-  --light-gray: #E8E8E8;
-  --snow-white: #F5F5F5;
-  --white: #F9F9F9;
+  --dark-gray: #5e5e5e;
+  --gray: #bfbfbf;
+  --light-gray: #e8e8e8;
+  --snow-white: #f5f5f5;
+  --white: #f9f9f9;
 }
 
 * {
--- src/components/UI/Modal.vue
+++ src/components/UI/Modal.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="modal">
+  <div class="modal" :class="{ active: open }">
     <section class="modal__container">
-      <span class="modal__close">
+      <span class="modal__close" @click="open = false">
         <XIcon />
       </span>
 
@@ -12,24 +12,14 @@
 
       <div class="modal__content">
         <ul>
-          <li>
-            <span>Name: </span> Squirtle
-          </li>
-          <li>
-            <span>Weight: </span> 20
-          </li>
-          <li>
-            <span>Height: </span> 18
-          </li>
-          <li>
-            <span>Types: </span> Normal, Water
-          </li>
+          <li><span>Name: </span> Squirtle</li>
+          <li><span>Weight: </span> 20</li>
+          <li><span>Height: </span> 18</li>
+          <li><span>Types: </span> Normal, Water</li>
         </ul>
 
         <div class="modal__btns">
-          <button-component>
-            Share to my friends
-          </button-component>
+          <button-component> Share to my friends </button-component>
 
           <button class="favorite__button">
             <StarIcon />
@@ -41,12 +31,18 @@
 </template>
 
 <script setup>
-import PikachuIcon from '../icons/PikachuIcon.vue';
+// import { ref } from 'vue';
+// import PikachuIcon from '../icons/PikachuIcon.vue';
 import StarIcon from '../icons/StarIcon.vue';
 import XIcon from '../icons/XIcon.vue';
 import ButtonComponent from './ButtonComponent.vue';
 
-
+const props = defineProps({
+  open: {
+    type: Boolean,
+    default: false,
+  },
+});
 </script>
 
 <style scoped>
@@ -63,17 +59,24 @@ import ButtonComponent from './ButtonComponent.vue';
   height: 100%;
   background-color: #00000099;
   z-index: 10;
+  transition: all 0.3s ease-in-out;
+  opacity: 0;
+  visibility: hidden;
+}
+.modal.active {
+  opacity: 1;
+  visibility: visible;
 }
 .modal__close {
   position: absolute;
   right: 0.938rem;
   top: 0.938rem;
-  color: #FFF;
+  color: #fff;
   cursor: pointer;
 }
 .modal__container {
   position: relative;
-  max-width: 520px;
+  max-width: 570px;
   width: 100vw;
 }
 .modal__hero {
@@ -88,7 +91,7 @@ import ButtonComponent from './ButtonComponent.vue';
 }
 .modal__content {
   padding: 1.25rem 1.875rem;
-  background-color: #FFF;
+  background-color: #fff;
 }
 .modal__content ul {
   padding: 0;
